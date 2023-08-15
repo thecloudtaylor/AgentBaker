b874eef1106cffd12b4c7d39dba52db22de26c08
Dallas Delaney <106280731+dallasd1@users.noreply.github.com>
2023-08-15 16:38:30 -0700

    Replace default uvm kernel path

--- vhdbuilder/packer/setupkata.sh
+++ vhdbuilder/packer/setupkata.sh
@@ -6,8 +6,9 @@ done
 
 sleep 10
 
-# replace the default kata uvm config path
+# replace the default kata uvm config paths
 sed -i -e "s|image = .*$|initrd = \"/var/cache/kata-containers/osbuilder-images/kernel-uvm/kata-containers-initrd.img\"|" /usr/share/defaults/kata-containers/configuration.toml
+sed -i -e "s|kernel = .*$|kernel = \"/usr/share/cloud-hypervisor/vmlinux.bin\"|" /usr/share/defaults/kata-containers/configuration.toml
 
 cat /etc/containerd/config.toml | grep kata > /dev/null
 if [[ $? != 0 ]]; then
